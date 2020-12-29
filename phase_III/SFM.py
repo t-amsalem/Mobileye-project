2242db2ea57adb65a0a8ef54f3d273dc3a10f827
T. Amsalem <tovaamsalem1@gmail.com>
2020-12-29 22:02:02 +0200

    Calaulate distance of the traffic lights

--- phase_III/SFM.py
+++ phase_III/SFM.py
@@ -2,80 +2,92 @@ from operator import itemgetter
 import numpy as np
 
 
-def calc_TFL_dist(prev_container, curr_container, focal, pp):
-    print("calc_TFL_dist")
-    print(curr_container.traffic_light)
+def calc_TFL_dist(prev_container, curr_container, focal: float, pp: np.ndarray):
     norm_prev_pts, norm_curr_pts, R, foe, tZ = prepare_3D_data(prev_container, curr_container, focal, pp)
+
     if abs(tZ) < 10e-6:
         print('tz = ', tZ)
+
     elif norm_prev_pts.size == 0:
         print('no prev points')
+
     elif norm_prev_pts.size == 0:
         print('no curr points')
+
     else:
-        curr_container.corresponding_ind, curr_container.traffic_lights_3d_location, curr_container.valid = calc_3D_data(norm_prev_pts, norm_curr_pts, R, foe, tZ)
+        curr_container.corresponding_ind, curr_container.traffic_lights_3d_location, curr_container.valid = \
+            calc_3D_data(norm_prev_pts, norm_curr_pts, R, foe, tZ)
+
     return curr_container
 
 
-def prepare_3D_data(prev_container, curr_container, focal, pp):
-    print("prepare_3D_data")
-    print(curr_container.traffic_light)
+def prepare_3D_data(prev_container, curr_container, focal: float, pp: np.ndarray):
     norm_prev_pts = normalize(prev_container.traffic_light, focal, pp)
     norm_curr_pts = normalize(curr_container.traffic_light, focal, pp)
     R, foe, tZ = decompose(np.array(curr_container.EM))
+
     return norm_prev_pts, norm_curr_pts, R, foe, tZ
 
 
-def calc_3D_data(norm_prev_pts, norm_curr_pts, R, foe, tZ):
+def calc_3D_data(norm_prev_pts: np.ndarray, norm_curr_pts: np.ndarray, R: np.ndarray, foe: np.ndarray, tZ: float) -> \
+        (list, np.array, list):
+
     norm_rot_pts = rotate(norm_prev_pts, R)
     pts_3D = []
     corresponding_ind = []
-    validVec = []
+    valid_vec = []
+
     for p_curr in norm_curr_pts:
         corresponding_p_ind, corresponding_p_rot = find_corresponding_points(p_curr, norm_rot_pts, foe)
         Z = calc_dist(p_curr, corresponding_p_rot, foe, tZ)
         valid = (Z > 0)
+
         if not valid:
             Z = 0
-        validVec.append(valid)
+        valid_vec.append(valid)
         P = Z * np.array([p_curr[0], p_curr[1], 1])
         pts_3D.append((P[0], P[1], P[2]))
         corresponding_ind.append(corresponding_p_ind)
-    return corresponding_ind, np.array(pts_3D), validVec
+
+    return corresponding_ind, np.array(pts_3D), valid_vec
 
 
-def normalize(pts, focal, pp):
+def normalize(pts: np.ndarray, focal: float, pp: np.ndarray) -> np.ndarray:
     return (pts - pp) / focal
 
 
-def unnormalize(pts, focal, pp):
+def unnormalize(pts: np.ndarray, focal: float, pp: np.ndarray) -> np.ndarray:
     return pts * focal + pp
 
 
-def decompose(EM):
+def decompose(EM: np.ndarray) -> (np.ndarray, float):
     R = EM[:3, :3]
     t = EM[:3, 3]
     tx, ty, tz = t[0], t[1], t[2]
     foe = np.array([tx/tz, ty/tz])
+
     return R, foe, tz
 
 
-def rotate(pts, R):
+def rotate(pts: np.ndarray, R: np.ndarray) -> [np.ndarray]:
     ones = np.ones((len(pts[:, 0]), 1), int)
     rotate_mat = np.dot(R, (np.hstack([pts, ones])).T)
+
     return (rotate_mat[:2]/rotate_mat[2]).T
 
 
-def find_corresponding_points(p, norm_pts_rot, foe):
+def find_corresponding_points(p: np.array, norm_pts_rot: list, foe: np.array) -> (int, np.ndarray):
     x, y = 0, 1
     m = (foe[y]-p[y])/(foe[x]-p[x])
     n = (p[y]*foe[x] - foe[y]*p[x])/(foe[x]-p[x])
+
     list_ = [[abs((m * pts[x] + n - pts[y]) / np.sqrt(pow(m, 2) + 1)), i] for i, pts in enumerate(norm_pts_rot)]
     min_dist, i_min = min(list_, key=itemgetter(0))
+
     return i_min, norm_pts_rot[i_min]
 
 
-def calc_dist(p_curr, p_rot, foe, tZ):
+def calc_dist(p_curr: np.ndarray, p_rot: np.ndarray, foe: np.array, tZ: float) -> float:
     dis_x = tZ * (foe[0] - p_rot[0]) / (p_curr[0] - p_rot[0])
     dis_y = tZ * (foe[1] - p_rot[1]) / (p_curr[1] - p_rot[1])
     m = abs(p_curr[1]-p_rot[1]) / abs((p_curr[0] - p_rot[0]))
--- phase_III/SFM_standAlone.py
+++ phase_III/SFM_standAlone.py
@@ -4,31 +4,36 @@ import matplotlib.pyplot as plt
 from phase_III import SFM
 
 
-def visualize(prev_container, curr_container, focal, pp):
+def visualize(prev_container, curr_container, focal: float, pp: np.ndarray) -> None:
     norm_prev_pts, norm_curr_pts, R, norm_foe, tZ = SFM.prepare_3D_data(prev_container, curr_container, focal, pp)
     norm_rot_pts = SFM.rotate(norm_prev_pts, R)
     rot_pts = SFM.unnormalize(norm_rot_pts, focal, pp)
+
     foe = np.squeeze(SFM.unnormalize(np.array([norm_foe]), focal, pp))
     fig, (curr_sec, prev_sec) = plt.subplots(1, 2, figsize=(12, 6))
+
     prev_sec.set_title('prev(' + str(prev_frame_id) + ')')
     prev_sec.imshow(prev_container.img)
     prev_p = prev_container.traffic_light
     prev_sec.plot(prev_p[:, 0], prev_p[:, 1], 'b+')
+
     curr_sec.set_title('curr(' + str(curr_frame_id) + ')')
     curr_sec.imshow(curr_container.img)
     curr_p = curr_container.traffic_light
     curr_sec.plot(curr_p[:, 0], curr_p[:, 1], 'b+')
+
     for i in range(len(curr_p)):
         curr_sec.plot([curr_p[i, 0], foe[0]], [curr_p[i, 1], foe[1]], 'b')
         if curr_container.valid[i]:
             curr_sec.text(curr_p[i, 0], curr_p[i, 1], r'{0:.1f}'.
                           format(curr_container.traffic_lights_3d_location[i, 2]), color='r')
+
     curr_sec.plot(foe[0], foe[1], 'r+')
     curr_sec.plot(rot_pts[:, 0], rot_pts[:, 1], 'g+')
     plt.show()
 
 
-class FrameContainer(object):
+class FrameContainer:
     def __init__(self, img_path):
         self.img = plt.imread(img_path)
         self.traffic_light = []
